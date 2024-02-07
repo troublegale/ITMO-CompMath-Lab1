ae881507b6b969a5801624b150ca36abf7b830bb
troublegale <eat.some.trouble@gmail.com>
2024-02-08 01:15:42 +0300

    added generate_random_matrix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from gauss import *
 from user_io import *
 from matrix_operations import row_echelon_form
+from utility import generate_random_matrix
 
 
-matrix = get_matrix_from_user_input()
+# matrix = get_matrix_from_user_input()
+matrix = generate_random_matrix()
 print()
 print_equation_system(matrix)
 print()
--- utility.py
+++ utility.py
@@ -2,5 +2,9 @@ import random
 
 
 def generate_random_matrix() -> list[list[float]]:
-    size = random.randint(2, 20)
-
+    size = random.randint(2, 5)
+    matrix = [[0.0] * (size + 1) for _ in range(size)]
+    for i in range(size):
+        for j in range(size + 1):
+            matrix[i][j] = random.randint(-10, 10)
+    return matrix
